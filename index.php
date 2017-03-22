3cdcddb1d3bb0812e3541f167ce1f6d5857c8933
ervinmuru <ervinmuru@mail.com>
2017-03-22 15:15:43 +0200

    created menu template files

--- index.php
+++ index.php
@@ -10,16 +10,17 @@
 require_once 'conf.php';
 
 // main page content
-echo '<h1>Veebiprogrammeerimine</h1>';
+echo '<h1>Web development</h1>';
 
 //creating main template object
 $main_tmpl = new template('main');
 //creating pairs template_element -> key_value
 $main_tmpl->set('user','Username');
-$main_tmpl->set('title','Main page');
+$main_tmpl->set('title','Page title');
 $main_tmpl->set('lang_bar','Language selection');
 $main_tmpl->set('menu','Page menu');
 $main_tmpl->set('content','Page content');
+$main_tmpl->set('site_title','Web development');
 //controlling object contents
 echo $main_tmpl->parse();
 ?>
