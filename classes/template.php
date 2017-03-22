ad70f112717273178f78a7d0d5b33b26802d4cce
ervinmuru <ervinmuru@mail.com>
2017-03-22 12:23:57 +0200

    created classes/template.php

---
+++ classes/template.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: darthuser
+ * Date: 3/22/17
+ * Time: 12:12 PM
+ */
+class template
+{//class start
+
+    //template class properties - variables
+    var $file = '';  //html template file name
+    var $content = false; //html template file content
+    var $vars = array(); //html view - real values
+    //class actions - methods - functions
+}//class end
