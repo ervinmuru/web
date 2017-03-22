f28668ce90e2580179f3bb185f6bd73d1de6a685
ervinmuru <ervinmuru@mail.com>
2017-03-22 15:31:14 +0200

    created menu 1st version

---
+++ menu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: darthuser
+ * Date: 3/22/17
+ * Time: 3:27 PM
+ */
+//creating menu template objects
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+
+echo '<pre>';
+print_r($menu);
+print_r($item);
+echo '<pre>';
+
+?>
