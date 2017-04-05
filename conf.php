25038c438a9f595eaf5be16cb9e5a1a9fe0ac1e8
ervinmuru <ervinmuru@mail.com>
2017-04-05 09:30:39 +0300

    Lisatud vaikimisi tegevuse faili nime konstant

--- conf.php
+++ conf.php
@@ -11,6 +11,7 @@ define('CLASSES_DIR', 'classes/'); // classes kataloogi nime konstant
 define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi nime konstant
 define('LIB_DIR', 'lib/'); // lib kataloogi nime konstant
 define('ACTS_DIR', 'acts/'); //acts kataloogi nime konstant
+define('DEFAULT_ACT','default'); //vaikimisi tegevuse faili nime konstant
 // võtame kasutusele vajalikud abifailid
 require_once LIB_DIR.'utils.php';
 // võtame kasutusele vajalikud failid
