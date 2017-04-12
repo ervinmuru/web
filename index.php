22cf6f7b7f112abd67f720760aa7b9e0c5b2a2e8
ervinmuru <ervinmuru@mail.com>
2017-04-12 12:02:21 +0300

    Kontrollitud aktiivne keel - hetkel vaikimisi määratud keel

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $main_tmpl = new template('main');
 // valmistame paarid malli_element => väärtus
 $main_tmpl->set('user', 'Kasutajanimi');
 $main_tmpl->set('title', 'Pealeht');
-$main_tmpl->set('lang_bar', 'Keeleriba');
+$main_tmpl->set('lang_bar', LANG_ID);Kontrollitud aktiivne keel - hetkel vaikimisi määratud keel
 $main_tmpl->set('menu', 'Lehe peamenüü');
 // kutsume menüü tööle testimiseks
 require_once 'menu.php';
