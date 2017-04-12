f68ca66ab799294f54d6292d6a3bc1f5f1b64a9d
ervinmuru <ervinmuru@mail.com>
2017-04-12 11:56:41 +0300

    Täiendatud keele näitamisega lingis

--- conf.php
+++ conf.php
@@ -11,15 +11,18 @@ define('CLASSES_DIR', 'classes/'); // classes kataloogi nime konstant
 define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi nime konstant
 define('LIB_DIR', 'lib/'); // lib kataloogi nime konstant
 define('ACTS_DIR', 'acts/'); // acts kataloogi nime konstant
+
+
 define('DEFAULT_ACT', 'default'); // vaikimisi tegevuse faili nime konstant
+define('DEFAULT_LANG', 'et'); // vaikimisi keele määramine
 
 // võtame kasutusele vajalikud abifailid
 require_once LIB_DIR.'utils.php';
 require_once 'db_conf.php';
 
-//keele toetus
+// lisame keele tugi
 $lang_id = DEFAULT_LANG;
-$http->set('lang_id',$lang_id);
+$http->set('lang_id', $lang_id);
 
 //vaikimise keele konstant
 define('DEFAULT_LANG', 'et');
