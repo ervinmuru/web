c3dc23f12020267f04be07579431cd09ea9e7cb0
ervinmuru <ervinmuru@mail.com>
2017-04-12 12:23:24 +0300

    Koostatud inglise keelse tõlge esmaversioon

---
+++ tmpl/lang/lang_en.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: darthuser
+ * Date: 4/12/17
+ * Time: 12:22 PM
+ */
+$_trans = array(
+    	'Esimene leht' => 'First page',
+    	'Teine leht' => 'Second page',
+    	'avaleht' => 'main page',
+    	'Lehe peamenüü' => 'Page menu'
+        );
