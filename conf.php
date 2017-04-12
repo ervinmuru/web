9d944596acd0baf69134df9ff7ecb1e855675836
ervinmuru <ervinmuru@mail.com>
2017-04-12 12:25:27 +0300

    Lisatud tõlkifuntksiooni kasutamine

--- conf.php
+++ conf.php
@@ -31,7 +31,7 @@ require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
 require_once CLASSES_DIR.'linkobject.php';
 require_once CLASSES_DIR.'mysql.php';
-
+require_once LIB_DIR.'trans.php'; // kutsume tõlkifunktsiooni asukoht
 
     );
 // loome vajalikud objektid projekti tööks
